2ca8d88174a5c2a93dbfa323985f0b73036578ea
최민재 <80940200+snack655@users.noreply.github.com>
2023-09-26 17:52:10 +0900

    Feature/core to placement (#3)
    * chore: add adcio core and analytics dependency in project
    
    * feat: remove logic about device, session id
    
    * chore: remove unused plugin (uuid, device info plus)
    
    * feat: add adcio_core deviceId and sessionId in client function
    
    * feat: add AdcioCore init to example code
    
    * test: remove adcio suggestion info in mock
    
    * feat: update example (used idOnStore)
    
    * test: add test of initilizeApp function time
    
    ---------
    
    Co-authored-by: adbr <adbr.dev@gmail.com>

--- lib/adcio_placement.dart
+++ lib/adcio_placement.dart
@@ -1,4 +1,4 @@
 library adcio_placement;
 
-export 'src/fetch_suggestion.dart' show adcioSuggest, getDeviceId, getSessionId;
+export 'src/fetch_suggestion.dart' show adcioSuggest;
 export 'src/api_result.dart';
